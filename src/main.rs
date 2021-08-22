7279933303c6b4adfa57c1adc0a2e1255ad48499
Alexander Nookinov <anookinov@gmail.com>
2021-08-22 14:07:43 +0700

    add reading files into string

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 use std::env;
 // use std::env::args;
 // use std::env::args_os; // accept arguments containing invalid Unicode
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,4 +14,9 @@ fn main() {
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
